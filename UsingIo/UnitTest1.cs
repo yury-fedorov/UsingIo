4ed86eba5f9a2d774b2ec40452abcef790d75ea5
FEDOROV Yury <fedoy@chzrhmac030.local>
2020-08-14 02:01:17 +0200

    example

---
+++ UsingIo/UnitTest1.cs
@@ -0,0 +1,40 @@
+using System.IO;
+using NUnit.Framework;
+
+namespace UsingIo
+{
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            var text = "ciao";
+            using (var ms = new MemoryStream())
+            {
+                // writing
+                // using var sw = new StreamWriter(ms);
+                using (var sw = new StreamWriter(ms)) 
+                {
+                    sw.WriteLine(text);
+                    sw.Flush();
+
+                    // first reading from the very beginning
+                    ms.Position = 0;
+                    using (var sr = new StreamReader(ms))
+                    {
+                        Assert.True(sr.ReadToEnd().StartsWith(text));
+                    }
+                    /* DOES NOT WORK BECAUSE THE STREAM IS CLOSED
+                    // second reading from the first byte skipped
+                    const int shift = 1;
+                    ms.Position = shift; // to check if ms is still open
+                    using (var sr = new StreamReader(ms))
+                    {
+                        Assert.True(sr.ReadToEnd().StartsWith(text.Substring(shift)));
+                    }
+                    */
+                }    
+            }
+        }
+    }
+}
