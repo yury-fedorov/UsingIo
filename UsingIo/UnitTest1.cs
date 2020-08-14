186c71ead6656c87b4c92a853c4001dba5d1cdc4
Yury Fedorov <yury.fedorov@gmail.com>
2020-08-14 11:26:08 +0200

    Update UnitTest1.cs
    The way how to fix XmlWriter call with MemoryStream

--- UsingIo/UnitTest1.cs
+++ UsingIo/UnitTest1.cs
@@ -1,4 +1,5 @@
 using System.IO;
+using System.Xml;
 using NUnit.Framework;
 
 namespace UsingIo
@@ -6,7 +7,7 @@ namespace UsingIo
     public class Tests
     {
         [Test]
-        public void Test1()
+        public void TestWithStreamWriter()
         {
             var text = "ciao";
             using (var ms = new MemoryStream())
@@ -33,7 +34,56 @@ namespace UsingIo
                         Assert.True(sr.ReadToEnd().StartsWith(text.Substring(shift)));
                     }
                     */
-                }    
+                }
+            }
+        }
+
+        [Test]
+        public void TestXmlWriter()
+        {
+            var text = "ciao";
+            using (var ms = new MemoryStream())
+            {
+                // writing
+                // using var sw = new StreamWriter(ms);
+                using (var sr = new StreamReader(ms))
+                {
+                    using (var sw = XmlWriter.Create(ms))
+                    {
+                        sw.WriteElementString(text, "hi!");
+                        sw.Flush();
+
+                        // first reading from the very beginning
+                        ms.Position = 0;
+
+                        Assert.True(sr.ReadToEnd().IndexOf(text) >= 0);
+                    }
+                }
+            }
+        }
+
+        [Test]
+        public void TestXmlWriterBroken()
+        {
+            var text = "ciao";
+            using (var ms = new MemoryStream())
+            {
+                // writing
+                using (var sw = XmlWriter.Create(ms))
+                {
+                    sw.WriteElementString(text, "hi!");
+                    sw.Close();
+                    // https://github.com/dotnet/runtime/blob/master/src/libraries/System.Private.Xml/src/System/Xml/Core/XmlWriter.cs
+                    // Dispose() calls Close() if the object is not yet closed
+                }
+
+                // first reading from the very beginning
+                ms.Position = 0;
+
+                using (var sr = new StreamReader(ms))
+                {
+                    Assert.True(sr.ReadToEnd().IndexOf(text) >= 0);
+                }
             }
         }
     }
